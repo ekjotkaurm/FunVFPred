08ba29ed6198d8dd1fcaa74ed1fb6401a7413851
ekjotkaurm <matharuekjot@gmail.com>
2025-04-08 16:11:56 +0530

    Update merge_aac_dde.py

--- Feature_fusion/merge_aac_dde.py
+++ Feature_fusion/merge_aac_dde.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 # List of CSV files to merge
 csv_files = [
-    "AAC.csv",
-    "DDE.csv"
+    "/FILES/AAC.csv",
+    "/FILES/DDE.csv"
 ]
 
 # Initialize merged_df with the first file
